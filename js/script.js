13c471545d08c6b5792c13c8e124129b70485026
NakhimoVV <tj1993@bk.ru>
2022-01-18 22:57:20 +0300

    fix: correction script.js #1

--- js/script.js
+++ js/script.js
@@ -1,32 +1,26 @@
 "use strict"
 
-function createNode(element) {
-    return document.createElement(element);
-}
+const beerDataName = document.querySelector('.card-beer__name');
+const beerDataBrand = document.querySelector('.card-beer__brand');
+const beerDataStyle = document.querySelector('.card-beer__style');
+const beerDataHop = document.querySelector('.card-beer__hop');
+const beerDataMalts = document.querySelector('.card-beer__malts');
+const beerDataAlcohol = document.querySelector('.card-beer__alcohol');
+const beerDataIbu = document.querySelector('.card-beer__ibu');
 
-function append(parent, el) {
-    return parent.appendChild(el);
-}
-
-const ul = document.getElementById('authors');
-const url = 'https://randomuser.me/api/?results=10';
+const url = 'https://random-data-api.com/api/beer/random_beer';
 
 fetch(url)
     .then((resp) => resp.json())
-    .then(function (data) {
-        let authors = data.results;
-        return authors.map(function (author) {
-            let li = createNode('li');
-            let img = createNode('img');
-            let span = createNode('span');
-            img.src = author.picture.medium;
-            span.innerHTML = `${author.name.first} ${author.name.last}`;
-            append(li, img);
-            append(li, span);
-            append(ul, li);
-        })
+    .then(beers => {
+        beerDataName.innerHTML = `${beers.name}`;
+        beerDataBrand.innerHTML = `${beers.brand}`;
+        beerDataStyle.innerHTML = `${beers.style}`;
+        beerDataHop.innerHTML = `${beers.hop}`;
+        beerDataMalts.innerHTML = `${beers.malts}`;
+        beerDataAlcohol.innerHTML = `${beers.alcohol}`;
+        beerDataIbu.innerHTML = `${beers.ibu}`;
     })
     .catch(function (error) {
-        console.log(error);
+        alert(error);
     });
-
