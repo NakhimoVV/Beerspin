0fdce902af1fb152714c736a4e6624edb7ccec55
NakhimoVV <tj1993@bk.ru>
2022-01-19 21:58:28 +0300

    fix: correction script.js #2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 "use strict"
 
+const userDataName = document.querySelector('.box-user__username');
+// const userDataAvatar = document.querySelector('.box-user__useravatar');
+
 const beerDataName = document.querySelector('.card-beer__name');
 const beerDataBrand = document.querySelector('.card-beer__brand');
 const beerDataStyle = document.querySelector('.card-beer__style');
@@ -8,19 +11,75 @@ const beerDataMalts = document.querySelector('.card-beer__malts');
 const beerDataAlcohol = document.querySelector('.card-beer__alcohol');
 const beerDataIbu = document.querySelector('.card-beer__ibu');
 
-const url = 'https://random-data-api.com/api/beer/random_beer';
+let tempBeer = {};
+let historyBeer = [];
 
-fetch(url)
-    .then((resp) => resp.json())
-    .then(beers => {
-        beerDataName.innerHTML = `${beers.name}`;
-        beerDataBrand.innerHTML = `${beers.brand}`;
-        beerDataStyle.innerHTML = `${beers.style}`;
-        beerDataHop.innerHTML = `${beers.hop}`;
-        beerDataMalts.innerHTML = `${beers.malts}`;
-        beerDataAlcohol.innerHTML = `${beers.alcohol}`;
-        beerDataIbu.innerHTML = `${beers.ibu}`;
-    })
-    .catch(function (error) {
-        alert(error);
-    });
+const url1 = 'https://random-data-api.com/api/users/random_user';
+const url2 = 'https://random-data-api.com/api/beer/random_beer';
+
+function changeUser() {
+    fetch(url1)
+        .then((resp) => resp.json())
+        .then(users => {
+            userDataName.innerHTML = `${users.username}`;
+            // userDataAvatar.innerHTML = `${users.avatar}`;
+        })
+        .catch(function (error) {
+            alert(error);
+        });
+}
+function clickSpin() {
+    setTimeout(() => {
+        fetch(url2)
+            .then((resp) => resp.json())
+            .then(beers => {
+                for (let key in beers) {
+                    switch (key) {
+                        case 'name':
+                        case 'brand':
+                        case 'style':
+                        case 'hop':
+                        case 'malts':
+                        case 'alcohol':
+                        case 'ibu':
+                            tempBeer[key] = beers[key];
+                        default:
+                            break;
+                    }
+                }
+                setTimeout
+                beerDataName.innerHTML = `${tempBeer.name}`;
+                beerDataBrand.innerHTML = `${tempBeer.brand}`;
+                beerDataStyle.innerHTML = `${tempBeer.style}`;
+                beerDataHop.innerHTML = `${tempBeer.hop}`;
+                beerDataMalts.innerHTML = `${tempBeer.malts}`;
+                beerDataAlcohol.innerHTML = `${tempBeer.alcohol}`;
+                beerDataIbu.innerHTML = `${tempBeer.ibu}`;
+            })
+            .catch(function (error) {
+                alert(error);
+            });
+    }, 1000);
+}
+function clickAccept() {
+    let clone = Object.assign({}, tempBeer);
+    //нужна проверка на равенство такого же объекта (2 x Accept)
+    // if (clone == historyBeer[historyBeer.length - 1]) break;
+    historyBeer.push(clone);
+    console.log(historyBeer);
+    console.log(clone);
+}
+
+userChange.onclick = changeUser;
+spin.onclick = clickSpin;
+accept.onclick = clickAccept;
+
+// let keys = ['a', 'b', 'c', 'd', 'e'];
+// let values = [1, 2, 3, 4, 5];
+// let obj = {};
+
+// for (let i = 0; i <= 4; i++) {
+//     obj[keys[i]] = values[i];
+// }
+
+// console.log(obj);
